c132d260c4edf45dba719d3d73c9c86c5b00e4e4
alvirtuoso <alohunk@yahoo.com>
2017-07-09 17:50:28 -0700

    changed port to 5000 from 3030 at Program.cs

--- AmazonProductSearch/Program.cs
+++ AmazonProductSearch/Program.cs
@@ -14,7 +14,7 @@ namespace AmazonProductSearch
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://127.0.0.1:3030")
+                .UseUrls("http://127.0.0.1:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
